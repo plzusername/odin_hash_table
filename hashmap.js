1e2b7724f2b575406cd451842ed57eea6f980ead
plzusername <MohamedFaris2210@gmail.com>
2024-04-26 15:07:47 +0300

    Finish work on hashmap implementation

--- hashmap.js
+++ hashmap.js
@@ -1,69 +1,148 @@
-const hashmap = () =>{
-    const hashMapLength = 16;
-    const loadFactor = 3 / 4
+const linkedList = require('../odin_linked_list/linkedLists')
+
+const node = (value = null, next = null) =>{
     return {
-        buckets:Array(hashMapLength).fill([]),
-        keys:[],
-        values:[],
+        value,
+        next
+    }
+}
+
+function fillInBuckets(size){
+    const hashBuckets = []
+
+    for (let i = 0; i < size; i++) {
+        hashBuckets.push(linkedList())
+    }
+
+    return hashBuckets
+}
+
+const hashmap = () =>{
+    const hashLength = 16;
+    const bucketsTemplate = fillInBuckets(hashLength)
+    return {
+        hashMapLength:hashLength,
+        unavailableBuckets:0,
+        propertiesLength:0,
+        entries:[],
+        buckets:bucketsTemplate,
         hash(key){
             let hashCode = 0;
-      
             const primeNumber = 31;
+
             for (let i = 0; i < key.length; i++) {
               hashCode = (primeNumber * hashCode + key.charCodeAt(i) ) % 16;
             }
          
-            return hashCode;         
+            return hashCode;
         },
         set(key, value){
             const bucketIndex = this.hash(key)
+            const linkedItemProperties = {key,value}
+            const listNode = node(linkedItemProperties)
+            const list = this.buckets[bucketIndex]
 
-            this.buckets[bucketIndex] = {hashCode: bucketIndex, key, value}
+            if(list.containsNode(linkedItemProperties.key, 'key').exists){
+                list.containsNode(linkedItemProperties.key, 'key').node.value.value = value
+                this.entries.find(entry => entry[0] == key)[1] = value
+                return 
+            }
+
+            this.propertiesLength += 1
+            this.unavailableBuckets += 1
+            this.entries.push([key, value])
+            list.listPrepend(listNode)
+
+            if(this.unavailableBuckets / this.hashMapLength >= 3 / 4){
+                for (let i = 0; i < this.hashMapLength; i++) {
+                    this.buckets.push(linkedList())
+                }
+                    this.hashMapLength *= 2
+            }
         },
         get(key){
             const bucketIndex = this.hash(key)
+            const list = this.buckets[bucketIndex]
 
-            return this.buckets[bucketIndex].value
+            return list.containsNode(key, 'key') == false ? null : list.containsNode(key, 'key').node.value.value
         },
-        has(key){
-            const bucketIndex = this.hash(key)
 
-            return this.buckets[bucketIndex].value != undefined
-        },
+        has : (key) =>  this.get(key) == null ? false : true ,
+
         clear(){
-            this.buckets = Array(hashMapLength).fill([])
+            this.hashMapLength = 16
+            this.propertiesLength = 0
             this.keys = []
             this.values = []
-            this.length = 0
-
+            this.buckets = []
         },
-        remove(key){
-            const bucketIndex = this.hash(key)
+        remove(key) {
+            const bucketIndex = this.hash(key);
+            const list = this.buckets[bucketIndex];
+        
+            let prev = null;
+            let current = list.headNode;
 
-            this.buckets[bucketIndex] = []
+            let lastCurrent = list.headNode
+        
+            while (current) {
+                if (current.value.key === key) {
+                    if (prev === null) {
+                        list.headNode = current.next;
+                    } else {
+                        prev.next = current.next;
+                    }
+                    const removedEntry = this.entries.find(entry => entry[0] == key)
+
+                    this.entries.splice(this.entries.indexOf(removedEntry),1)
+                    this.propertiesLength--;
+                    break;
+                }
+                prev = current;
+                current = current.next;
+            }
+
+            while(lastCurrent){
+                if(lastCurrent.next == null){
+                    list.tailNode = lastCurrent
+                }
+                lastCurrent = lastCurrent.next
+            }
+        },
+        getKeys() {
+            return this.entries.map(entry => entry[0]);
+        },
+        getValues() {
+            return this.entries.map(entry => entry[1]);
+        },
+        getEntries() {
+            return this.entries;
         },
         getLength(){
-
-        },
-        getValues(){
-
-        },
-        getKeys(){
-
-        },
-        getEntries(){
+            return this.propertiesLength
             
         }
+
+
 
     }
 }
 
 const myHashMap = hashmap()
 
-myHashMap.set('John', 12)
-myHashMap.set('Jake', 13)
-myHashMap.set('Mary', 124)
-myHashMap.set('Chloe', 1324)
-myHashMap.set('Ahmed', 1224)
+myHashMap.set('John', 83)
+myHashMap.set('Mary', 81)
+myHashMap.set('Msdfary', 81)
+myHashMap.set('Maffry', 81)
+myHashMap.set('Mafsdfry', 81)
+myHashMap.set('Mary', 81)
+myHashMap.set('Mfsdfary', 81)
+myHashMap.set('Mafry', 81)
+myHashMap.set('Ma5ry', 81)
+myHashMap.set('Mdfary', 81)
+myHashMap.set('6Marsdfy', 81)
+myHashMap.set('M45ary', 81)
+myHashMap.set('M6ary', 81)
+myHashMap.set('M78ary', 813)
 
 console.log(myHashMap.buckets)
